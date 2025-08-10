8a2baf57ff89db9f86b2d3e12ff4d1513ca438d4
Ferid Babayev <feridbabayevv08@gmail.com>
2025-08-10 15:13:07 +0400

    Create esp32_energy_meter.ino

---
+++ devices-code/esp32_energy_meter.ino
@@ -0,0 +1,65 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* mqtt_server = "broker.emqx.io";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32EnergyMeter")) {
+      Serial.println("connected");
+      // subscribe if needed
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Simulyasiya üçün təsadüfi enerji dəyəri
+  float power_usage = random(300, 400);
+
+  char msg[50];
+  snprintf(msg, 50, "%.2f", power_usage);
+  client.publish("energy/consumption", msg);
+
+  delay(5000);
+}
